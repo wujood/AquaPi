fb56a51b1906fac3df30aefba794c31b7b8021b7
Thomas Wu <thomas@acerian>
2018-05-29 15:44:35 +0200

    analog input read working

--- deploy_and_run.py
+++ deploy_and_run.py
@@ -1,6 +1,5 @@
 import os
-RPI_IP='192.168.2.235'
+RPI_IP='192.168.8.234'
 
 os.system('scp sensors.py pi@'+RPI_IP+':/home/pi/')
-os.system('ssh pi@'+RPI_IP+' "python3 sensors.py"')
-
+os.system('ssh -t pi@'+RPI_IP+' "python3 sensors.py"')
--- sensors.py
+++ sensors.py
@@ -1,43 +1,108 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import threading
 import time
 import sys
+import spidev
+from statistics import median
+
 sys.path.insert(0, './Client')
-import swagger_client
+# ~ import swagger_client
 
-# ~ #How much time between sensor-reads in seconds
-# ~ PERIOD_WATER_LVL=10
-# ~ PERIOD_BRIGHTNESS=60
+### GPIO SETUP ###
+## digital peripherals
+GPIO.setmode(GPIO.BCM)
+#Relais for light (using Normally-Open-mode (NO))
+RELAY_LIGHT=12
+GPIO.setup(RELAY_LIGHT, GPIO.OUT)
+GPIO.output(RELAY_LIGHT, GPIO.LOW)
+#Light barrier
+INP_LIGHT_BARRIER=27
+GPIO.setup(INP_LIGHT_BARRIER, GPIO.IN)
+## analog peripherals
+#Water level sensor
+INP_WATERLVL=3
+#Brightness sensor
+INP_BRIGHTNESS=7
 
-# ~ ### GPIO SETUP ###
-# ~ GPIO.setmode(GPIO.BCM)`
-# ~ #Water level sensor
-# ~ INP_WATERLVL_GPIO=4
-# ~ GPIO.setup(INP_WATERLVL_GPIO, GPIO.IN)
-# ~ #Brightness sensor
-# ~ INP_BRIGHTNESS_GPIO=17
-# ~ GPIO.setup(INP_BRIGHTNESS_GPIO, GPIO.IN)
-# ~ #Relais for light (using Normally-Open-mode (NO))
-# ~ RELAY_LIGHT_GPIO=27
-# ~ GPIO.setup(RELAY_LIGHT_GPIO, GPIO.OUT)
-# ~ GPIO.output(RELAY_LIGHT_GPIO, GPIO.LOW)
+mosipin=10
+misopin=9
+cspin=8
+clockpin=11
+GPIO.setup(mosipin, GPIO.OUT)
+GPIO.setup(misopin, GPIO.IN)
+GPIO.setup(cspin, GPIO.OUT)
+GPIO.setup(clockpin, GPIO.OUT)
 
-client=swagger_client.api_client.ApiClient()
-client.
+def readadc(adcnum):
+	if ((adcnum > 7) or (adcnum < 0)):
+			return -1
+	GPIO.output(cspin, True)  
 
+	GPIO.output(clockpin, False)  # start clock low
+	GPIO.output(cspin, False)     # bring CS low
 
-# ~ def read_water_lvl():
+	commandout = adcnum
+	commandout |= 0x18  # start bit + single-ended bit
+	commandout <<= 3    # we only need to send 5 bits here
+	for i in range(5):
+		if (commandout & 0x80):
+				GPIO.output(mosipin, True)
+		else:
+				GPIO.output(mosipin, False)
+		commandout <<= 1
+		GPIO.output(clockpin, True)
+		GPIO.output(clockpin, False)
+
+	adcout = 0
+	# read in one empty bit, one null bit and 10 ADC bits
+	for i in range(12):
+		GPIO.output(clockpin, True)
+		GPIO.output(clockpin, False)
+		adcout <<= 1
+		if (GPIO.input(misopin)):
+			adcout |= 0x1
+
+	GPIO.output(cspin, True)
 	
-	# ~ threading.Timer(PERIOD_WATER_LVL,read_water_lvl).start()
+	adcout >>= 1       # first bit is 'null' so drop it
+	return adcout
 
-# ~ def read_brightness():
-	
-	# ~ threading.Timer(PERIOD_BRIGHTNESS,read_brightness).start()
 
-# ~ def switch_light(on):
-	# ~ if on:
-		# ~ GPIO.output(RELAY_LIGHT_GPIO, GPIO.HIGH)
-	# ~ else:
-		# ~ GPIO.output(RELAY_LIGHT_GPIO, GPIO.LOW)
+sample_size=50
+sensor_data=[None]*sample_size
+def smooth_sensor_read(analog_input_pin):
+	for i in range(sample_size):
+		sensor_data[i]=readadc(analog_input_pin)
+	return median(sensor_data)
 
-# ~ GPIO.cleanup()  
+spi = spidev.SpiDev()
+spi.open(0,0)
+# ~ client=swagger_client.api_client.ApiClient()
+def read_brightness():
+	return smooth_sensor_read(INP_BRIGHTNESS)
+
+def read_temperature():
+	pass
+
+def read_water_lvl():
+	return smooth_sensor_read(INP_WATERLVL)
+
+def switch_light(on):
+	if on:
+		GPIO.output(RELAY_LIGHT, GPIO.HIGH)
+	else:
+		GPIO.output(RELAY_LIGHT, GPIO.LOW)
+		
+def read_light_barrier():
+	return GPIO.input(INP_LIGHT_BARRIER)
+
+try:
+	while 1:
+		time.sleep(0.5)
+		# ~ print('Helligkeit: ',read_brightness())
+		print(read_water_lvl())
+		# ~ print(read_light_barrier())
+except KeyboardInterrupt:
+    pass
+finally:
+	GPIO.cleanup()  
