3e07a802e7548928de3cf07db71845c59d7137d5
Sebastian Weiß <wujood@gmx.de>
2018-07-03 11:13:11 +0000

    Removed Server folder

---
+++ launch.sh
@@ -0,0 +1,1 @@
+python3 sensors.py
--- sensors.py
+++ sensors.py
@@ -11,18 +11,42 @@ sys.path.insert(0, './Client')
 ### GPIO SETUP ###
 ## digital peripherals
 GPIO.setmode(GPIO.BCM)
+
 #Relais for light (using Normally-Open-mode (NO))
 RELAY_LIGHT=12
 GPIO.setup(RELAY_LIGHT, GPIO.OUT)
 GPIO.output(RELAY_LIGHT, GPIO.LOW)
+
+#SERVO
+OUT_SERVO=26
+GPIO.setup(OUT_SERVO, GPIO.OUT)
+GPIO.output(OUT_SERVO, GPIO.LOW)
+
 #Light barrier
-INP_LIGHT_BARRIER=27
+INP_LIGHT_BARRIER=6
 GPIO.setup(INP_LIGHT_BARRIER, GPIO.IN)
-## analog peripherals
+
+#LED 
+OUT_LED=2
+GPIO.setup(OUT_LED, GPIO.OUT)
+GPIO.output(OUT_LED, GPIO.HIGH)
+
+#RELAY
+OUT_RELAY=4
+GPIO.setup(OUT_RELAY, GPIO.OUT)
+GPIO.output(OUT_RELAY, GPIO.HIGH)
+
+### ANALOG ###
+
+#Temperature
+INP_TEMP_WATER=0
+INP_TEMP_AIR=2
+
 #Water level sensor
-INP_WATERLVL=3
+INP_WATERLVL=5
+
 #Brightness sensor
-INP_BRIGHTNESS=7
+INP_BRIGHTNESS=6
 
 mosipin=10
 misopin=9
@@ -81,8 +105,11 @@ spi.open(0,0)
 def read_brightness():
 	return smooth_sensor_read(INP_BRIGHTNESS)
 
-def read_temperature():
-	pass
+def read_temperature_air():
+	return smooth_sensor_read(INP_TEMP_AIR)
+
+def read_temperature_water():
+	return smooth_sensor_read(INP_TEMP_WATER)
 
 def read_water_lvl():
 	return smooth_sensor_read(INP_WATERLVL)
@@ -94,14 +121,15 @@ def switch_light(on):
 		GPIO.output(RELAY_LIGHT, GPIO.LOW)
 		
 def read_light_barrier():
-	return GPIO.input(INP_LIGHT_BARRIER)
+	return GPIO.input(INP_TEMP)
 
 try:
 	while 1:
 		time.sleep(0.5)
-		# ~ print('Helligkeit: ',read_brightness())
-		print(read_water_lvl())
-		# ~ print(read_light_barrier())
+		print('Helligkeit: ',read_brightness())
+		print('Wasserstand:  ',read_water_lvl())
+		print('Lufttemperatur: ',read_temperature_air())
+		print('Wassertemperatur: ',read_temperature_water())
 except KeyboardInterrupt:
     pass
 finally:
